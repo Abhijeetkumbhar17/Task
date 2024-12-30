9e287a314a178429010f700bfb8c02f9468510f0
Abhijeetkumbhar17 <1711abhijeetk@gmail.com>
2024-12-30 11:57:35 +0530

    Update layout.blade.php

--- resources/views/tasks/layout.blade.php
+++ resources/views/tasks/layout.blade.php
@@ -4,6 +4,7 @@
     <title>Task Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 </head>
 <body>
       
@@ -12,4 +13,13 @@
 </div>
       
 </body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+<script>
+  $(function() {
+    $("#inputDate").datepicker({
+      minDate: 0 // 0 means today
+    });
+  });
+</script>
 </html>
